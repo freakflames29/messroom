cab6f34235f383daa139cbd10c92d4d72ef3f7d9
Sourav Das <iamsouravdaswade420@gmail.com>
2022-05-16 00:36:31 +0530

    added some js

---
+++ app/javascript/packs/myjs.js
@@ -0,0 +1,13 @@
+console.log("messroom")
+let or = document.querySelector("body > div.container > div.hero_container > div.hero_search > form > span:nth-child(3)")
+let city = document.querySelector("#search_city")
+
+function changeor () {
+	if(city.value.length===0)
+	{
+		or.textContent='or';
+	}
+	or.textContent='and';
+}
+
+city.addEventListener('keypress', changeor)
