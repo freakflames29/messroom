a50e22f6dd48e4f9c2ffd62a2b851f5f3d8e991d
Sourav Das <iamsouravdaswade420@gmail.com>
2022-05-16 23:58:46 +0530

    added esc close option

--- app/javascript/packs/float_search.js
+++ app/javascript/packs/float_search.js
@@ -18,5 +18,19 @@ function showsearch()
 
 
 }
+function closesearchesc(e)
+{
+	if(e.keyCode==27)
+	{
+		blckdiv.classList.add('hide')
+		srchdiv.classList.add('hide')
+	}
+}
 searchbtn.addEventListener('click',showsearch)
 blckdiv.addEventListener('click',closesearch)
+
+window.onkeydown = function( event ) {
+    if ( event.keyCode == 27 ) {
+        closesearchesc(event);
+    }
+};
