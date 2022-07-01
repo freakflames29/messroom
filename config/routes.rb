afc4bc48d709f59204f5696220d4b9b0ae5246c1
Sourav Das <iamsouravdaswade420@gmail.com>
2022-07-01 21:53:01 +0530

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   resources :users,except:[:new]
   get 'signup',to:'users#new'
   # resources :pins
-  # get 'random/new'
+  # get 'random/new'  
   # post 'find', to:'random#find'
   root 'welcome#index'
 
